f38a8d0b8b8f365fc128d7ab00ec56ae1b80ecd2
kakaheryan <kakaheryan@gmail.com>
2021-11-26 10:01:26 +0700

    fixed error

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+
+import 'react-tippy/dist/tippy.css';
+import '@/styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+
+        <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
